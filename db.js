752526d9f9e9bf544d2b897449ab1461806a5a68
Alexis4952 <alexandrosronis@gmail.com>
2025-03-05 20:08:21 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: "localhost",
+    user: "root",
+    password: "",  // Άφησε το κενό αν δεν έχεις βάλει κωδικό
+    database: "fixnow_db"
 });
 
 db.connect((err) => {
     if (err) {
         console.error("Database connection failed: ", err);
-    } else {
-        console.log("Connected to MySQL Database");
+        return;
     }
+    console.log("Connected to database!");
 });
 
 module.exports = db;
+
